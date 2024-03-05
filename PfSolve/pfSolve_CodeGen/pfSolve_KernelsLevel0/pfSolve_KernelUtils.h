e7be52feee87afedf9900c2468e3cc09dfa77666
Dmitrii Tolmachev <dtolm96@gmail.com>
2024-03-05 18:44:17 +0100

    added missing sharedMatricesUpload

--- PfSolve/pfSolve_CodeGen/pfSolve_KernelsLevel0/pfSolve_KernelUtils.h
+++ PfSolve/pfSolve_CodeGen/pfSolve_KernelsLevel0/pfSolve_KernelUtils.h
@@ -84,7 +84,7 @@ static inline void appendExtensions(PfSolveSpecializationConstantsLayout* sc) {
 #include <%s/include/cuda_fp16.h>\n", CUDA_TOOLKIT_ROOT_DIR);
 		PfAppendLine(sc);
 	}
-	if(sc->num_warps_data_parallel>1){
+	if((sc->num_warps_data_parallel>1) && (sc->sharedMatricesUpload)){
 		sc->tempLen = sprintf(sc->tempStr, "\
 	#include <%s/include/cuda_pipeline.h>\n", CUDA_TOOLKIT_ROOT_DIR);
 		PfAppendLine(sc);
